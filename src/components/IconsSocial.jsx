44e57b73dfa1351eb026b6a525c7f3e679cfa7db
Iroro1 <ojigboleo@gmail.com>
2023-01-20 11:43:29 +0100

    Social Icons

--- src/components/IconsSocial.jsx
+++ src/components/IconsSocial.jsx
@@ -1,10 +1,38 @@
-import { DocumentScanner } from '@mui/icons-material'
-import React from 'react'
+import { CreditCardOutlined, EmailRounded, GitHub } from "@mui/icons-material";
+import { Document, Celsius } from "iconsax-react";
+import React from "react";
+import { FaLinkedinIn } from "react-icons/fa";
 
-const IconsSocial = ({name}) => {
+const IconsSocial = ({ name, link }) => {
   return (
-    <div><DocumentScanner/></div>
-  )
-}
+    <div className="cursor-pointer">
+      <a href={link} download>
+        {name === "Cv" && <Document variant="Bold" color="#6D9FFF" size={25} />}
+      </a>
+      <a href={link}>
+        {name === "Credly" && (
+          <Celsius variant="Bold" color="#0E4B3D" size={25} />
+        )}
+        {name === "LinkedIn" && <FaLinkedinIn size={25} color={"#3F63A9"} />}
+        {name === "Github" && (
+          <GitHub
+            sx={{
+              width: "25px",
+              color: "#1E1E1E",
+            }}
+          />
+        )}
+        {name === "Gmail" && (
+          <EmailRounded
+            sx={{
+              color: "#F33D3D",
+              width: "25px",
+            }}
+          />
+        )}
+      </a>
+    </div>
+  );
+};
 
-export default IconsSocial
+export default IconsSocial;

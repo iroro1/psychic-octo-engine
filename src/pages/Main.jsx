ba344859bf6788b4a6add2abe618c06c9d4ecf28
Iroro1 <ojigboleo@gmail.com>
2023-01-21 12:28:39 +0100

    KKK

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
-import Home from './Home'
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Home";
 const Main = () => {
   return (
     <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Home/>} />
-    </Routes>
+      <Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;

eac0fa4acae107a16a8b6eb0785a2a9747a6d15e
Iroro1 <ojigboleo@gmail.com>
2023-01-21 12:30:34 +0100

    KKK

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,6 +5,7 @@ const Main = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
